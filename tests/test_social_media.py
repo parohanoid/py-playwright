74a14c858d631e4ff7f22f77ff80ea3829136df3
parohanoid <rohanpanchal76@gmail.com>
2025-11-02 19:53:11 +0530

    users as a list

--- tests/test_social_media.py
+++ tests/test_social_media.py
@@ -7,24 +7,30 @@ from pages.signup_page import SignupPage
 from pytest_csv_params.decorator import csv_params
 import requests
 
-@pytest.mark.parametrize("users", ["John", "Emma", "Liam"])
-def test_like_and_notification(page: Page, social_media_url, users):
+# @pytest.mark.parametrize("users", ["John", "Emma", "Liam"])
+def test_like_and_notification(page: Page, social_media_url: str):
     page.goto(social_media_url)
 
-    text = page.locator(f"xpath=//h6[contains(text(), '{users}')]/../..//button/following-sibling::p").inner_text()
-    initial_likes = int(text.split(" likes")[0])
+    users = ["John", "Emma", "Liam"]
 
-    initial_notifications = int(page.locator("xpath=//*[contains(@class,'MuiBadge-anchorOriginTopRightRectangular')]").inner_text() or 0)
+    for user in users:
 
-    # Simulate user liking a post
-    page.locator(f"xpath=//h6[contains(text(), '{users}')]/../..//button").click()
+        text = page.locator(f"xpath=//h6[contains(text(), '{user}')]/../..//button/following-sibling::p").inner_text()
+        initial_likes = int(text.split(" likes")[0])
 
-    expect(page.locator(f"xpath=//h6[contains(text(), '{users}')]/../..//button/following-sibling::p")).to_have_text(f"{str(initial_likes + 1)} likes")
+        initial_notifications = int(page.locator("xpath=//*[contains(@class,'MuiBadge-anchorOriginTopRightRectangular')]").inner_text() or 0)
 
-    # Simulate user receiving a notification
-    expect(page.locator("xpath=//*[contains(@class,'MuiBadge-anchorOriginTopRightRectangular')]")).to_have_text(str(initial_notifications + 1))
+        # Simulate user liking a post
+        page.locator(f"xpath=//h6[contains(text(), '{user}')]/../..//button").click()
 
-    # Click on the notification and verify the message
-    page.locator("xpath=//*[contains(@class,'MuiBadge-anchorOriginTopRightRectangular')]/ancestor::button").click()
+        expect(page.locator(f"xpath=//h6[contains(text(), '{user}')]/../..//button/following-sibling::p")).to_have_text(f"{str(initial_likes + 1)} likes")
 
-    expect(page.locator("xpath=//h6[contains(text(), 'Notifications')]/following-sibling::*//p").filter(has_text=f"You liked {users}'s post")).to_be_visible()
+        # Simulate user receiving a notification
+        expect(page.locator("xpath=//*[contains(@class,'MuiBadge-anchorOriginTopRightRectangular')]")).to_have_text(str(initial_notifications + 1))
+
+        # Click on the notification and verify the message
+        page.locator("xpath=//*[contains(@class,'MuiBadge-anchorOriginTopRightRectangular')]/ancestor::button").click()
+
+        expect(page.locator("xpath=//h6[contains(text(), 'Notifications')]/following-sibling::*//p").filter(has_text=f"You liked {user}'s post")).to_be_visible()
+
+        page.keyboard.press("Escape")
